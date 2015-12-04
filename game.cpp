bf54070c48225cbd42464f39e282104f7f2faaad
Nihal Singh <nihal.111@gmail.com>
2015-12-04 14:17:48 +0530

    Rotation function

--- game.cpp
+++ game.cpp
@@ -3,6 +3,9 @@
 #include <SFML/Graphics/Image.hpp>
 #include <math.h>
 
+sf::Vector2i mouse_pos;
+
+
 void isPlaneClicked ( sf::Sprite *spr, sf::RenderWindow *render , sf::Vector2i mouse_pos )
 {
     if   ( mouse_pos.x > spr->getPosition().x
@@ -18,6 +21,26 @@ void isPlaneClicked ( sf::Sprite *spr, sf::RenderWindow *render , sf::Vector2i m
     }
 }
 
+void rotate (sf::Sprite *planeSprite, sf::Vertex vertex)
+{	sf::Vector2f point = vertex.position;
+    sf::Vector2f plane_pos;
+    double rise, run, angle;
+	planeSprite->setOrigin(24,24);
+	plane_pos=planeSprite->getPosition();
+
+    //std::cout<<"Mouse: "<<mouse_pos.x<<" "<<mouse_pos.y<<std::endl;
+    //std::cout<<"Plane: "<<plane_pos.x<<" "<<plane_pos.y<<std::endl;
+    rise= point.y - (plane_pos.y+24);
+    run = point.x - (plane_pos.x+24);
+    angle = 57.296*atan(double(rise/run));
+    std::cout<<"Rise: "<<rise<<"Run: "<<run<<"Angle: "<<angle<<std::endl;
+    if (run<0)
+      	planeSprite->setRotation(135+angle+180);
+    else
+    	planeSprite->setRotation(135+angle);
+
+
+}
 
 int main()
 {
@@ -38,7 +61,7 @@ int main()
 	bg.setTexture(texture);
 
 
-    sf::Vector2i mouse_pos;
+    
     sf::Vector2f plane_pos;
 
 
@@ -52,23 +75,12 @@ int main()
     
     planeSprite.setPosition(100,100);
 
-    double rise, run, angle;
+ 
 
     while (window.isOpen())
     {	mouse_pos=sf::Mouse::getPosition(window);
-    	plane_pos=planeSprite.getPosition();
-
-        std::cout<<"Mouse: "<<mouse_pos.x<<" "<<mouse_pos.y<<std::endl;
-        std::cout<<"Plane: "<<plane_pos.x<<" "<<plane_pos.y<<std::endl;
-        rise= mouse_pos.y - (plane_pos.y+24);
-        run = mouse_pos.x - (plane_pos.x+24);
-
-        angle = 57.296*atan(double(rise/run));
-        std::cout<<"Rise: "<<rise<<"Run: "<<run<<"Angle: "<<angle<<std::endl;
-        if (run<0)
-        	planeSprite.setRotation(135+angle+180);
-    	else
-    		planeSprite.setRotation(135+angle);
+    	
+    	//rotate(&planeSprite);
 
         sf::Event event;
         while (window.pollEvent(event))

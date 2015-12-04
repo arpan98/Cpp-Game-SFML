755e6e7f918dfa612ae9de64d671618e0d0a1eb1
Arpan Banerjee <arpanbnrj9@gmail.com>
2015-12-04 14:17:22 +0530

    line on mouse drag

--- game.cpp
+++ game.cpp
@@ -1,27 +1,63 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
+#include <utility>
+#include <math.h>
 #include <SFML/Graphics/Image.hpp>
 
+#define coords std::pair<int,int>
 
-void isPlaneClicked ( sf::Sprite *spr, sf::RenderWindow *render , sf::Vector2i mouse_pos )
+std::vector<sf::Vector2i> mouse_coords(500);
+
+sf::Vertex line_points[500];
+sf::Vertex line_vertices[2];
+
+
+bool isPlaneClicked ( sf::Sprite *spr, sf::RenderWindow *render , sf::Vector2i mouse_pos )
 {
     if   ( mouse_pos.x > spr->getPosition().x
         && mouse_pos.x < spr->getPosition().x + 48
         && mouse_pos.y > spr->getPosition().y
         && mouse_pos.y < spr->getPosition().y + 48 )
     {
-        std::cout<<"Click\n";
+        return true;
     }
     else
     {
-        std::cout<<"NO\n";
+        return false;
     }
 }
+
+int distance (long index , sf::Vector2i mouse_pos)
+{
+    return fabs(mouse_pos.y- mouse_coords[index].y) + fabs(mouse_pos.x - mouse_coords[index].x);
+}
+
+void drawLine(long ctr , sf::RenderWindow *window)
+{
+    if(ctr>=1)
+    {
+        for(long i=0 ; i<ctr ; i++)
+        {
+            line_vertices[0]=line_points[i-1];
+            line_vertices[1]=line_points[i];
+            window->draw(line_vertices , 2 , sf::Lines);
+        }
+    }
+}
+
 
 
 int main()
 {
     sf::RenderWindow window;
+    sf::VertexArray lines(sf::LinesStrip, 500);
+    sf::Vector2i initial_point;
+
+    long ctr=0;
+    int dist;
+    bool planeClicked = false;
+    bool click_start = false;
 
     window.create(sf::VideoMode (774,359), "Game");
     window.setPosition(sf::Vector2i(100,100));
@@ -49,6 +85,7 @@ int main()
 
     while (window.isOpen())
     {
+        mouse_pos=sf::Mouse::getPosition(window);
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -63,21 +100,47 @@ int main()
                 default :
                     break;
             }
+
         }
+
+
 
         if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
         {
-            mouse_pos=sf::Mouse::getPosition(window);
-            isPlaneClicked(&planeSprite,&window,mouse_pos);
+            if(click_start==false && planeClicked==false)
+                planeClicked=isPlaneClicked(&planeSprite,&window,mouse_pos);
+
+            click_start=true;
+
+            if(planeClicked && ctr==0) {
+                mouse_coords[ctr].x=mouse_pos.x;
+                mouse_coords[ctr].y=mouse_pos.y;
+                line_points[ctr]=sf::Vertex(sf::Vector2f(mouse_pos.x , mouse_pos.y));
+                ctr++; 
+            }
+            else if(planeClicked)
+            {
+                //std::cout<<mouse_pos.x<<" "<<mouse_pos.y<<'\n';
+                dist = distance(ctr-1,mouse_pos);
+                if(dist>50) {
+                    //mouse_coords.push_back(std::make_pair(mouse_pos.x , mouse_pos.y));
+                    mouse_coords[ctr].x=mouse_pos.x;
+                    mouse_coords[ctr].y=mouse_pos.y;
+
+                    lines[ctr-1].position = sf::Vector2f(mouse_pos.x , mouse_pos.y);
+                    lines[ctr-1].color = sf::Color::Black;
+
+                    line_points[ctr]=sf::Vertex(sf::Vector2f(mouse_pos.x , mouse_pos.y));
+                    
+                    ctr++;
+                }
+            }
         }
 
-        window.clear();
 
         window.draw(bg);
-
         window.draw(planeSprite);
-        
-
+        drawLine(ctr-1 , &window);
         window.display();
     }
 
